b4b485a495b792eab3adba9c7427fe463216e084
Garang-Samuel <113675952+Garang-Samuel@users.noreply.github.com>
2022-09-30 12:26:58 +0300

    initial commit

---
+++ GitHubEclipseDemonstration/src/Samuel/Mary.java
@@ -0,0 +1,14 @@
+package Samuel;
+
+public class Mary 
+{
+	public static void main(String[]args)
+	{
+		int a = 1000;
+		int b = 2000;
+		int c = a + b;
+		
+		System.out.println("|The Final Addition Answer is:| " + c);
+	}
+
+}
